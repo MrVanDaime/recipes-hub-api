8db8bd565d7aaf956e7e090575ac9697dedd52ed
MrVanDaime - Home <joaocamini@live.com>
2024-07-04 09:53:41 -0300

    Added server module

--- src/index.js
+++ src/index.js
@@ -16,6 +16,8 @@ require("./startup/cors")(app);
 require("./startup/routes")(app);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log( `Listening on port ${PORT}` );
 });
+
+module.exports = server;
